a21e534f4ed8e9da64545b2b751a6ef131a6e0d5
Mauricio <maurio.aravena@sansano.usm.cl>
2022-06-28 11:26:17 -0400

    Avances GUI

--- lis_corral_gomez_implementation/gui.py
+++ lis_corral_gomez_implementation/gui.py
@@ -1,58 +1,38 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
-from processing_options import argsHandler
-from processing import smokepoint
-# Default values
-CORE_THRESHOLD_PERCENT_TEXT = 75
-CONTOUR_THRESHOLD_PERCENT_TEXT = 35
-DEFAULT_VIDEO="test.avi"
-def startProcessing(args, core, contour, display, bb):
-    core_field = core.get()
-    contouer_field = contour.get()
-    args.TresholdCore = float(core_field)
-    args.ThreshodContour = float(contouer_field)
-    args.Display = display.get()
-    args.Boxes = bb.get()
-    # ONLY FOR DEMO
-    args.Video = DEFAULT_VIDEO
-    print(args)
-    smokepoint(args)
+import ttkthemes
+from tkinter.filedialog import askopenfile
+
+class  MainWindow:
 
 
-def basicGUI():
-    args = argsHandler()
-    root = Tk()
-    root.title("Smoke Point Detection")
-    processing_params_frame = ttk.Frame(root, padding=10)
-    processing_params_frame.grid()
-    # Text for processing section
-    ttk.Label(processing_params_frame, text="Parámetros de Procesamiento").grid(column=0, row=0)
-    ttk.Label(processing_params_frame, text="Umbral Nucleo").grid(column=0,row=1)
-    ttk.Label(processing_params_frame, text="Umbral Contorno").grid(column=0,row=2)
-    ttk.Label(processing_params_frame, text="%").grid(column=3,row=1)
-    ttk.Label(processing_params_frame, text="%").grid(column=3,row=2)
-    # Fields
-    core_threshold_field = ttk.Entry(processing_params_frame,width=5)
-    core_threshold_field.grid(column=1,row=1)
-    # Default core Threshold
-    core_threshold_field.insert(0,CORE_THRESHOLD_PERCENT_TEXT)
-    contour_threshold_field = ttk.Entry(processing_params_frame,width=5)
-    contour_threshold_field.grid(column=1,row=2)
-    # Default contouer threshold
-    contour_threshold_field.insert(0,CONTOUR_THRESHOLD_PERCENT_TEXT)
-    ttk.Button(processing_params_frame, text="Quit", command=root.destroy).grid(column=1, row=0)
-    # Checkboxes
-    # -> Vars
-    display_var = BooleanVar()
-    bb_var = BooleanVar()
-    # -> Actual boxes
-    bb_box = ttk.Checkbutton(processing_params_frame, text="BoundingBoxes", offvalue=False, onvalue=True, variable=bb_var)
-    bb_box.grid(column=1, row=3)
-    display_box = ttk.Checkbutton(processing_params_frame, text="Display", offvalue=False, onvalue=True, variable=display_var)
-    display_box.grid(column=0,row=3)
+    #Constructor
+    def __init__(self):
+        self.root = tk.Tk()
+        # Define UI style
+        self.root.style = ttkthemes.ThemedStyle()
+        self.root.style.theme_use('breeze')
 
-    ttk.Button(processing_params_frame, text="Start", command=lambda: startProcessing(args,core_threshold_field, contour_threshold_field, display_var, bb_var)).grid(column=1,row=5)
-    root.mainloop()
+        self.filePane = FileBox(self.root)
 
+    def start(self):
+        self.root.mainloop()
+
+class FileBox:
+
+    def __init__(self, root):
+        self.btn = tk.Button(root, text='Open', command = lambda:self.openFile())
+        self.btn.pack(side=tk.TOP)
+
+
+    def openFile(self):
+        file = askopenfile(mode='r', filetypes = [('Python Files', '*.py')])
+
+
+
+def main():
+    print("Hello World!")
+    a = MainWindow()
+    a.start()
 if __name__ == '__main__':
-    basicGUI()
+    main()
