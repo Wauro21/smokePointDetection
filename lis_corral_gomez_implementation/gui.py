c23141500bc71b4a577967c1b4b2fdc3e4877ba7
Mauricio <maurio.aravena@sansano.usm.cl>
2022-06-30 01:19:46 -0400

    Avances en GUI

--- lis_corral_gomez_implementation/gui.py
+++ lis_corral_gomez_implementation/gui.py
@@ -4,10 +4,26 @@ import ttkthemes
 from tkinter.filedialog import askopenfilename
 import os
 import cv2
+import imutils
+from PIL import Image, ImageTk
+import time
+import threading
+
+def readFrames(frameHolder):
+    video = cv2.VideoCapture('test.mp4')
+    while video.isOpened():
+        ret, frame = video.read()
+        if(ret):
+            cv2img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(cv2img)
+            imgTk = ImageTk.PhotoImage(image=img)
+            #frameHolder.imgtk = imgTk
+            frameHolder.config(image=imgTk)
+            time.sleep(1/25)
+        else:
+            break
 
 class  MainWindow:
-
-
     #Constructor
     def __init__(self):
         self.root = tk.Tk()
@@ -16,7 +32,7 @@ class  MainWindow:
         self.root.style.theme_use('breeze')
 
         self.filePane = FileBox(self.root)
-        print(vars(self.filePane))
+        self.videoPane = VideoWindow(self.root)
 
     def start(self):
         self.root.mainloop()
@@ -35,13 +51,18 @@ class FileBox(ttk.Frame):
 
         self.fileName = tk.StringVar(self, 'EMPTY FILE')
         self.filePath = tk.StringVar(self, 'EMPTY PATH')
+        self.infoText = tk.StringVar(self, '')
 
         input_sign = tk.Label(self, text='Input: ',font='Helvetica 10 bold')
         input_sign.grid(row = 0, column = 0)
 
-        self.file_display = tk.Label(self, textvariable=self.fileName,width=30, justify='left',anchor='w',relief='sunken')
+        self.file_display = tk.Label(self, textvariable=self.fileName,width=30, justify='left',anchor='w',relief='sunken', bg='white')
         self.file_display.grid(row=0, column=1)
-        #self.
+
+        self.info_title = tk.Label(self, text = 'Media Information:',font='Helvetica 10 bold')
+        self.info_title.grid(row=1,column=0)
+        self.info_display = tk.Label(self, textvariable=self.infoText, justify='left')
+        self.info_display.grid(row=2, column=0)
 
         self.btn = tk.Button(self, text='Open', command = self.loadRoutine )
         self.btn.grid(row = 0, column = 2)
@@ -63,23 +84,31 @@ class FileBox(ttk.Frame):
 
 
     def getInfo(self):
+        base_text = "- Dimentions: {} px x {} px \n- FPS: {} \n- Frames totales: {}"
 
         try:
-            print(self.filePath.get())
             video = cv2.VideoCapture(self.filePath.get())
             width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
             height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
             fps = video.get(cv2.CAP_PROP_FPS)
-            fc = video.get(cv2.CAP_PROP_FRAME_COUNT)
+            fc = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
             video.release()
-            print('DIM {}x{}, fps {}, fc {}'.format(width,height, fps, fc))
+            self.infoText.set(base_text.format(width, height, fps, fc))
         except Exception as e:
-            print("AN ERROR NOT IMPLEMENTED YET ...")
-            print(e)
+            tk.messagebox.showerror('Media Error', e)
+
+class VideoWindow(ttk.Frame):
+    def __init__(self,root):
+        super().__init__(root, relief=tk.GROOVE, borderwidth = 1)
+        self.videoDisplay = tk.Label(self, width=500, height=500)
+        self.videoDisplay.grid(row=0,column=0)
+        #self.videoDisplay.imgtk = None
+        self.pack()
 
 def main():
-    print("Hello World!")
     a = MainWindow()
+    x = threading.Thread(target=readFrames, args=(a.videoPane.videoDisplay,), daemon=True)
+    x.start()
     a.start()
 if __name__ == '__main__':
     main()
