cfee13abfc25978f6ec0eb4108176a2b7fd6a1e0
Mauricio <maurio.aravena@sansano.usm.cl>
2023-03-19 03:58:55 -0300

    Fixed the size of the window

--- SPD.py
+++ SPD.py
@@ -1,3 +1,23 @@
 # Main handler for GUI/terminal calls
-
 import sys
+import os
+from PyQt5.QtWidgets import QApplication
+from gui.MainWindow import SPDMain
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    if os.name == 'nt':
+        app.setStyle('Fusion')
+    window = SPDMain()
+    window.show()
+    # For the moment fix the size
+    h = window.height()
+    w = window.width()
+    window.setFixedSize(w, h)
+    sys.exit(app.exec())
