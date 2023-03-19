4eb0923794fe7960dd656a6c3f69058a7c36f3f9
Mauricio <maurio.aravena@sansano.usm.cl>
2023-03-19 13:07:04 -0300

    Added handler for gui/terminal. Core still needs cleaning

--- SPD.py
+++ SPD.py
@@ -2,22 +2,36 @@
 import sys
 import os
 from PyQt5.QtWidgets import QApplication
+from core.processing import smokepoint
+from core.processing_options import argsHandler
 from gui.MainWindow import SPDMain
+from gui.utils import resultPlotting
 
 
 
+def CMDsp(args):
+    sp_vals = smokepoint(args)
+    print('Invalid Frames : {}'.format(sp_vals['n_invalid_frames']))
+    resultPlotting(sp_vals,True)
 
 
 
 
 if __name__ == '__main__':
-    app = QApplication([])
-    if os.name == 'nt':
-        app.setStyle('Fusion')
-    window = SPDMain()
-    window.show()
-    # For the moment fix the size
-    h = window.height()
-    w = window.width()
-    window.setFixedSize(w, h)
-    sys.exit(app.exec())
+
+    args = argsHandler()
+
+    if(args.cmd):
+        CMDsp(args)
+
+    else:
+        app = QApplication([])
+        if os.name == 'nt':
+            app.setStyle('Fusion')
+        window = SPDMain()
+        window.show()
+        # For the moment fix the size
+        h = window.height()
+        w = window.width()
+        window.setFixedSize(w, h)
+        sys.exit(app.exec())
--- core/processing_options.py
+++ core/processing_options.py
@@ -1,5 +1,5 @@
 import argparse
-
+import sys
 # Default values
 DEFAULT_CORE_THRESHOLD=75.0#35.0
 DEFAULT_CONTOUR_THRESHOLD = 35.0#2.5
@@ -14,10 +14,10 @@ class Range(object):
         return self.start <= other <= self.end
 
 def argsHandler():
-        # [FIX]: Change the description of the algorithm
-        dsc = "Implements the algorithm described in the paper"
+        dsc = "Smoke Point Detection (SPD) is a python-based software that implements an algorithm that allows to perform smoke point characterization of fuel blends tested on the ASTM-D1322 Standarized Lamps. The software uses images taken during the lamp tests and allows to automatically detect the smoke point."
         parser = argparse.ArgumentParser(description=dsc)
-        parser.add_argument("-i", "--Input", required=True, help="Input video or image folder to process")
+        parser.add_argument("-cmd", action='store_true', help="Only terminal mode enabled")
+        parser.add_argument("-i", "--Input", required='-cmd' in sys.argv, help="Input video or image folder to process")
         # [FIX]: Add image processing, like external frames
         parser.add_argument("-d", "--Display", action='store_true', help="Displays the processing process")
         parser.add_argument("-tcore", "--ThresholdCore", default=DEFAULT_CORE_THRESHOLD, type=float, help="Percentage of the max value to use as threshold for the core region. Default is {}".format(DEFAULT_CORE_THRESHOLD))
@@ -30,7 +30,8 @@ def argsHandler():
         parser.add_argument("-cut", help="Cut the frames columnwise from center axis", type=int)
         parser.add_argument("--Verbose", action='store_true', help='If passed enables information prints through terminal.')
         parser.add_argument("--saveFig",help="Saves figures used in thesis.", type=int)
-        parser.add_argument('--runName', help="Name for the data resulting from the run", type=str,required=True)
+        # Remove following arg
+        parser.add_argument('--runName', help="Name for the data resulting from the run", type=str,required='-cmd' in sys.argv)
         args = parser.parse_args()
         return args
 
